41d8583221b9b856e048c4b26e0be20b7d7b80df
Richard A. Castaneda <castaneda.r@husky.neu.edu>
2021-03-23 22:21:04 -0700

    Functionailty complete on A5

--- wbdv-sp21-02-castanedar-server-java/src/main/java/com/example/wbdvsp2102castanedarserverjava/controllers/WidgetController.java
+++ wbdv-sp21-02-castanedar-server-java/src/main/java/com/example/wbdvsp2102castanedarserverjava/controllers/WidgetController.java
@@ -1,12 +1,13 @@
-package com.example.wbdvsp2102castanedarserverjava.controlers;
+package com.example.wbdvsp2102castanedarserverjava.controllers;
 import com.example.wbdvsp2102castanedarserverjava.models.Widget;
 import com.example.wbdvsp2102castanedarserverjava.services.WidgetService;
-import java.util.Date;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
@@ -22,7 +23,7 @@ public class WidgetController {
       @PathVariable("tid")String topicId,
       @RequestBody Widget widget
   ) {
-    return service.createWidgetForTopic(topicId, widget);
+    return service.createWidget(topicId, widget);
   }
 
   @GetMapping("/api/widgets")
@@ -30,10 +31,24 @@ public class WidgetController {
     return service.findAllWidgets();
   }
 
-  @GetMapping("/api/topics/{tid}/widget")
+
+ @PutMapping("/api/widgets/{wid}")
+    public Integer updateWidget(
+        @PathVariable ("wid") Long Id,
+        @RequestBody Widget widget){
+    return service.updateWidget(Id, widget);
+  }
+
+  @GetMapping("/api/topics/{tid}/widgets")
   public List<Widget> findWidgetForTopic (
       @PathVariable("tid") String topicId
   ) {
     return service.findWidgetsForTopic(topicId);
+  }
+
+@DeleteMapping("/api/widgets/{wid}")
+public Integer deleteWidget(
+  @PathVariable("wid") Long Id) {
+  return service.deleteWidget(Id);
   }
 }
--- wbdv-sp21-02-castanedar-server-java/src/main/java/com/example/wbdvsp2102castanedarserverjava/services/WidgetService.java
+++ wbdv-sp21-02-castanedar-server-java/src/main/java/com/example/wbdvsp2102castanedarserverjava/services/WidgetService.java
@@ -4,47 +4,63 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import org.springframework.stereotype.Service;
-//CRUD operations for Widgets
 
 @Service
 public class WidgetService {
-
   private List<Widget> widgets = new ArrayList<Widget>();
-
   {
-    Widget w1 = new Widget(123l, "ABC123", "HEADING", 1, "Welcome to Widget List");
-    Widget w2 = new Widget(234l, "ABC234", "Test2", 1, "Welcome to Widget List");
-    Widget w3 = new Widget(345l, "ABC345", "Test3", 1, "Welcome to Widget List");
-    Widget w4 = new Widget(456l, "ABC456", "Test4", 1, "Welcome to Widget List");
-    Widget w5 = new Widget(567l, "ABC567", "Test5", 1, "Welcome to Widget List");
-    Widget w6 = new Widget(678l, "ABC678", "PARAGRAPH", 1, "Welcome to Widget List");
+    Widget w1 = new Widget(123l, "ABC123", "HEADING", 2, "Hello From Widget 1! ABC123");
+    Widget w2 = new Widget(234l, "ABC123", "PARAGRAPH", 1, "Hello From Widget 2! ABC123 ");
+    Widget w3 = new Widget(345l, "ABC234", "HEADING", 1, "Hello From Widget 3! ABC234");
+    Widget w4 = new Widget(456l, "ABC234", "PARAGRAPH", 1, "Hello From Widget 4! ABC234");
     widgets.add(w1);
     widgets.add(w2);
     widgets.add(w3);
     widgets.add(w4);
-    widgets.add(w5);
-    widgets.add(w6);
   }
-
-  public Widget createWidgetForTopic(String topicId, Widget widget) {
-  widget.setTopicId(topicId);
-  widget.setId((new Date()).getTime());
-  widget.setType("HEADING");
-  widgets.add(widget);
-  return widget;
-  }
-
 
   public List<Widget> findAllWidgets() {
     return widgets;
   }
 
-  public List<Widget> findWidgetsForTopic(String topicId) {
+  public Widget createWidget(String tid, Widget widget) {
+  widget.setTopicId(tid);
+  widget.setId((new Date()).getTime());
+  widgets.add(widget);
+  return widget;
+  }
+
+  public Integer updateWidget(Long wid, Widget widget) {
+    for (Widget w : widgets) {
+      if (w.getId() == wid) {
+        int index = widgets.indexOf(w);
+        widgets.set(index, widget);
+        return 1;
+      } else {
+        return -1;
+      }
+    }
+    return 0;
+  }
+
+  public List<Widget> findWidgetsForTopic(String tid) {
     List<Widget> ws = new ArrayList<Widget>();
     for (Widget w : widgets) {
-      if (w.getTopicId().equals(topicId))
+      if (w.getTopicId().equals(tid))
         ws.add(w);
     }
     return ws;
+  }
+
+  public Integer deleteWidget(Long id) {
+    int index = -1;
+    for(int i=0; i<widgets.size(); i++) {
+      if (widgets.get(i).getId() == id) {
+        index = i;
+        widgets.remove(index);
+        break;
+      }
+    }
+    return -1;
   }
 }
